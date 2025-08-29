6778df47f26d3ffd747c1e425d7c4857fb54179b
Ajay Kulkarni <ajaykulkarni178@gmail.com>
2025-08-30 00:49:24 +0530

    Fixed the errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, 'useState', 'useEffect' } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Linkedin, Github, Menu, X, Briefcase, ArrowUpRight, Download, Eye, ArrowUp, GraduationCap } from 'lucide-react';
 
 // --- TYPE DEFINITIONS ---
